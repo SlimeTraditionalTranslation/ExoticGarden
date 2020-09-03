7d34c4b6ba8107da4f2127634d6f59ede89b3b8b
weiii-joe <49436544+weiii-joe@users.noreply.github.com>
2020-09-03 18:35:01 +0800

    翻譯懶懶wei

--- src/main/java/io/github/thebusybiscuit/exoticgarden/ExoticGardenRecipeTypes.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/ExoticGardenRecipeTypes.java
@@ -11,9 +11,9 @@ public final class ExoticGardenRecipeTypes {
 
     private ExoticGardenRecipeTypes() {}
 
-    public static final RecipeType KITCHEN = new RecipeType(new NamespacedKey(ExoticGarden.instance, "kitchen"), new SlimefunItemStack("KITCHEN", Material.CAULDRON, "&eKitchen"), "", "&rThis item must be made", "&rin a Kitchen");
-    public static final RecipeType BREAKING_GRASS = new RecipeType(new NamespacedKey(ExoticGarden.instance, "breaking_grass"), new CustomItem(Material.GRASS, "&7Breaking Grass"));
-    public static final RecipeType HARVEST_TREE = new RecipeType(new NamespacedKey(ExoticGarden.instance, "harvest_tree"), new CustomItem(Material.OAK_LEAVES, "&aHarvesting a Tree", "", "&rYou can obtain this Item by", "&rharvesting the shown Tree"));
-    public static final RecipeType HARVEST_BUSH = new RecipeType(new NamespacedKey(ExoticGarden.instance, "harvest_bush"), new CustomItem(Material.OAK_LEAVES, "&aHarvesting a Bush", "", "&rYou can obtain this Item by", "&rharvesting the shown Bush"));
+    public static final RecipeType KITCHEN = new RecipeType(new NamespacedKey(ExoticGarden.instance, "kitchen"), new SlimefunItemStack("KITCHEN", Material.CAULDRON, "&e廚房(Kitchen)"), "", "&r該物品必須在廚房裡製作");
+    public static final RecipeType BREAKING_GRASS = new RecipeType(new NamespacedKey(ExoticGarden.instance, "breaking_grass"), new CustomItem(Material.GRASS, "&7破壞雜草"));
+    public static final RecipeType HARVEST_TREE = new RecipeType(new NamespacedKey(ExoticGarden.instance, "harvest_tree"), new CustomItem(Material.OAK_LEAVES, "&a採收一棵樹", "", "&r可以通過種植特定樹木", "&r來獲得該物品"));
+    public static final RecipeType HARVEST_BUSH = new RecipeType(new NamespacedKey(ExoticGarden.instance, "harvest_bush"), new CustomItem(Material.OAK_LEAVES, "&a採收一株灌木", "", "&r你可以通過種植特定灌木叢", "&r來獲得該物品"));
 
 }
